9248511a75be88555bc4b22f74ac1d7938250bd3
lindsaykatz <lindsaykatz16@gmail.com>
2025-01-19 16:55:47 -0500

    add complete assignment script

---
+++ 02_activities/assignments/assignment_complete.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -x
+
+############################################
+# DSI CONSULTING INC. Project setup script #
+############################################
+# This script creates standard analysis and output directories
+# for a new project. It also creates a README file with the
+# project name and a brief description of the project.
+# Then it unzips the raw data provided by the client.
+
+mkdir analysis output
+touch README.md
+touch analysis/main.py
+
+# download client data
+curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+unzip rawdata.zip
+
+###########################################
+# Complete assignment here
+
+# 1. Create a directory named data
+mkdir data
+
+# 2. Move the ./rawdata directory to ./data/raw
+cd data
+mv ../rawdata raw
+
+# 3. List the contents of the ./data/raw directory
+ls raw
+
+# 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir processed
+mkdir processed/server_logs processed/user_logs processed/event_logs
+
+# 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cp raw/*server*.log processed/server_logs
+
+# 6. Repeat the above step for user logs and event logs
+cp raw/*user*.log processed/user_logs
+cp raw/*event*.log processed/event_logs
+
+# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm raw/*ipaddr*.log
+rm processed/user_logs/*ipaddr*.log
+
+# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+touch inventory.txt
+ls processed/* >> inventory.txt
+
+###########################################
+
+echo "Project setup is complete!"
